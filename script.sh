41e8ae5c7e51203bfcb28f7dc484b4db2e123e49
eniolastyle <amiolastyle@gmail.com>
2023-04-14 09:47:12 +0100

    memories app server

--- script.sh
+++ script.sh
@@ -13,32 +13,8 @@ sudo apt-get update
 
 # install nodejs and npm
 sudo apt install -y nodejs
-sudo apt install npm -y
+sudo apt install -y npm
 
-
-echo "
-----------------------
-  MONGODB
-----------------------
-"
-
-# import mongodb 4.0 public gpg key
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-
-# create the /etc/apt/sources.list.d/mongodb-org-4.0.list file for mongodb
-# echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-
-# reload local package database
-# sudo apt-get update
-
-# install the latest version of mongodb
-# sudo apt-get install -y mongodb-org
-
-# start mongodb
-# sudo systemctl start mongod
-
-# set mongodb to start automatically on system startup
-# sudo systemctl enable mongod
 
 
 echo "
@@ -46,11 +22,6 @@ echo "
   PM2
 ----------------------
 "
-# pm2 start app.js --env production
-#   pm2 status
-#   pm2 save
-#   pm2 startup 
-#   pm2 stop all
 
 # install pm2 with npm
 sudo npm install -g pm2 -y
